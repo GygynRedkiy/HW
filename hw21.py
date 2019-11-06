d71c880449fde7aeae6d2cb9e284149024a63ed3
GygynRedkiy <55867873+GygynRedkiy@users.noreply.github.com>
2019-11-06 22:48:24 +0500

    Update hw21.py

--- hw21.py
+++ hw21.py
@@ -1,8 +1,8 @@
-# Определить значение функции Z = 1 / (XY) при появлении X и Y.
-импортировать случайный
-A =  1
-B =  100
-C =  int (random.randint (A, B))
-D =  int (random.randint (A, B))
-F =  1  / (C * D)
-print ( " Функция равна " , F)
+# Определить значение функции Z = 1/(XY ) при произвольных X и Y .
+import random
+A = 1
+B = 100
+C = int(random.randint(A, B))
+D = int(random.randint(A, B))
+F = 1 / (C*D)
+print("Функция равна ",F)
